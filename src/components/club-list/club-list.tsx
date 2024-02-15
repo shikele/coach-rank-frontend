4bcd554424c1e29533cb24ddd60dfc66aca793a5
shike <shikele123456@gmail.com>
2024-02-15 14:38:33 -0800

    side bar finished, main page with background img

--- src/components/club-list/club-list.tsx
+++ src/components/club-list/club-list.tsx
@@ -4,6 +4,7 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import './club-list.scss';
 
 const teamName = ["A", "B", "C", "D", "E"]
 
@@ -15,7 +16,7 @@ export const ClubList = () => {
           return (
 
             <ListItem key={teamName} disablePadding>
-              <ListItemButton>
+              <ListItemButton className={"TeamButton"}>
                 <ListItemText primary={teamName} />
               </ListItemButton>
             </ListItem>
--- src/components/side-bar/side-bar.tsx
+++ src/components/side-bar/side-bar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -13,6 +12,12 @@ import { CgGenderFemale } from "react-icons/cg";
 import { SlCalender } from "react-icons/sl";
 import { PiSoccerBall } from "react-icons/pi";
 import {ClubList} from "../club-list/club-list";
+import { IoMenu } from "react-icons/io5";
+import { MdArrowBackIosNew } from "react-icons/md";
+import logo from "../../resources/logo.png"
+import logo2 from "../../resources/logo2.png"
+
+import './side-bar.scss';
 
 const sideMenu = [
     { title: 'Home', path: '/home', Icon: HomeIcon },
@@ -30,33 +35,29 @@ export default function TemporaryDrawer() {
 
     const toggleDrawer =
         (anchor: Anchor, open: boolean) =>
-            (event: React.KeyboardEvent | React.MouseEvent) => {
-                if (
-                    event.type === 'keydown' &&
-                    ((event as React.KeyboardEvent).key === 'Tab' ||
-                        (event as React.KeyboardEvent).key === 'Shift')
-                ) {
-                    return;
-                }
-
+            () => {
                 setState({ ...state, [anchor]: open });
             };
 
     const list = (anchor: Anchor) => (
         <Box
-            sx={{ width: 250 }}
+            className={"Box"}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
         >
-            <div> Title </div>
-            <List>
+            <div>
+                <img src={logo2} alt="header image" className="SideMenuHeaderIcon" />
+                <span className={"SideMenuReturnIconSpan"} onClick={toggleDrawer("left", false)}>
+                    <MdArrowBackIosNew className={"SideMenuReturnIcon"}/>
+                </span>
+
+            </div>
+            <List className="SideMenuList">
                 {sideMenu.map((menuItem, index) => {
                     const { title, path, Icon } = menuItem
                     return (
 
-                        <ListItem key={title} disablePadding>
-                            <ListItemButton>
+                        <ListItem key={title} disablePadding className="ListItem">
+                            <ListItemButton className="ListItemButton">
                                 <ListItemIcon>
                                     <Icon />
                                 </ListItemIcon>
@@ -75,10 +76,20 @@ export default function TemporaryDrawer() {
     );
 
     return (
-        <div>
+        <div className={"HeaderRow"}>
             {(['left'] as const).map((anchor) => (
                 <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                    <div onClick={toggleDrawer(anchor, true)} className="SideMenuOpenDiv">
+                        <IoMenu className="SideMenuOpenButton"/>
+                    </div>
+                    <div className="MenuHeaderIconDiv">
+                        <img src={logo} alt="header logo"/>
+                    </div>
+                    <div className={"ContactUsBox"}>
+                        <span>Contact Us</span>
+                    </div>
+
+
                     <Drawer
                         anchor={anchor}
                         open={state[anchor]}
