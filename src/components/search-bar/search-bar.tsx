2a852d59595e762dc6d1bd816befbea5f99eca09
shike <shikele123456@gmail.com>
2024-02-28 11:40:50 -0800

    working on toggle

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -8,7 +8,7 @@ import {
     Autocomplete,
     Button,
     createFilterOptions,
-    TextField,
+    TextField, ToggleButton, ToggleButtonGroup,
 
 } from "@mui/material";
 
@@ -24,11 +24,26 @@ export default function SearchBar() {
 
     const uniqueCoachNames = new Set()
     const uniqueTeamNames = new Set()
+    const uniqueClubNames = new Set()
 
     coachFullData.forEach((item) => {
         uniqueCoachNames.add(item.coach_name);
         uniqueTeamNames.add(item.team_name);
+        uniqueClubNames.add(item.club_name)
     });
+
+    const [alignment, setAlignment] = React.useState('web');
+
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        selection: string,
+    ) => {
+        if (selection !== null) {
+            setAlignment(selection);
+        }
+        console.log(selection)
+    };
+
     const mergedUniqueNames = Array.from(new Set([...uniqueCoachNames, ...uniqueTeamNames]));
     return (
         <Autocomplete
@@ -38,7 +53,7 @@ export default function SearchBar() {
             id="searchAutoComplete"
             options={mergedUniqueNames}
             sx={{
-                width: '80%',
+                width: '70%',
                 '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
                     borderRadius: '30px',
                 },
@@ -66,7 +81,31 @@ export default function SearchBar() {
                     <TextField {...params}
                                placeholder="Search for the coach name, or team name to find a suitable coach"
                                 className={"SearchTextField"}/>
-                    <Button variant="contained" className={"inter-bold SearchBarButton"}>Search</Button>
+                    <div className={"ButtonsDiv"}>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={alignment}
+                            exclusive
+                            onChange={handleChange}
+                            aria-label="Platform"
+                            sx={{
+                                '.MuiToggleButtonGroup-grouped': {
+                                    margin: '0px',
+                                    border: '#252525 solid',
+                                    '&.Mui-selected': {
+                                        backgroundColor: 'lightblue',
+                                        color: 'black',
+                                    }
+                                },
+                            }}
+                        >
+                            <ToggleButton value="coach">Coach name</ToggleButton>
+                            <ToggleButton value="team">Team name</ToggleButton>
+                            <ToggleButton value="club">Club name</ToggleButton>
+                        </ToggleButtonGroup>
+                        <Button variant="contained" className={"inter-bold SearchBarButton"}>Search</Button>
+                    </div>
+
                 </div>
                 }
             componentsProps={{
